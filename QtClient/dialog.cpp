19d22a24c72e9584675447b50bd8b11fdd0b4b3e
Shubham Verma <shubham.saint@gmail.com>
2017-09-15 13:15:33 +0530

    Client Server
    Completed

---
+++ QtClient/dialog.cpp
@@ -0,0 +1,50 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+#include <QMessageBox>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    socket = new QTcpSocket(this);
+     connect(socket, SIGNAL(readyRead()), this, SLOT(readServer()));
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButtonConnect_clicked()
+{
+    socket->connectToHost(ui->textEditIP->toPlainText(),ui->textEditPort->toPlainText().toInt());
+}
+
+void Dialog::on_pushButtonSend_clicked()
+{
+
+
+       QString message = ui->textEditMessage->toPlainText().trimmed();
+       if(!message.isEmpty())
+       {
+           socket->write(QString(message + " ").toUtf8());
+       }
+
+        ui->textEditMessage->clear();
+}
+
+void Dialog::readServer()
+{
+    QTcpSocket *socket = (QTcpSocket*)sender();
+
+   // msg.append(ui->textEdit_chat->toPlainText());
+
+    while(socket->canReadLine())
+    {
+         msg.append(socket->readLine());
+    }
+
+    ui->textEdit_chat->setText(msg);
+}
